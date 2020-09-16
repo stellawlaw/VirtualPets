a77d0fd0df64ee248bb2e3a40b4d9f728de41029
Lauren Monowar-Jones <lvjones214@aol.com>
2020-09-16 11:41:39 -0400

    ask user for a name

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -1,5 +1,16 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class Application {
+    public static void main (String[]args){
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("What is your pet's name?");
+        String name = input.nextLine();
+        System.out.println("Your pet's name is " + name);
+    }
+
+
 
 }
