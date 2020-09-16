df4ec1dd7c85bcbe5378c46691ebae68bf29fc38
Lauren Monowar-Jones <lvjones214@aol.com>
2020-09-16 11:08:09 -0400

    Pet has a name

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,12 @@
+package virtual_pet;
+
+public class VirtualPet {
+    public VirtualPet(String name) {
+    }
+    public String getName() {
+        return "Jeff";
+    }
+//    public int getHunger =10;
+
+
+}
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,4 +1,25 @@
 package virtual_pet;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetTest {
+
+    @Test
+    public void shouldBeAbleToCreatePet() {
+        VirtualPet pet = new VirtualPet("Test Name");
+    }
+    @Test
+    public void petShouldHaveName() {
+        VirtualPet underTest = new VirtualPet("Jeff");
+        String result = underTest.getName();
+        assertEquals("Jeff", result);
+    }
+//    @Test
+//    public void shouldHaveDefaultHunger() {
+//        VirtualPet underTest = new VirtualPet("Steve");
+//        int result = underTest.getHunger;
+//        assertEquals(10, result);
+//  }
 }
