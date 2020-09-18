dccf985f50ca870abc757ff4311939b4e000c2cd
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-09-18 11:48:54 -0400

    Created test

--- src/main/java/virtual_pet/VirtualPetShelter.java
+++ src/main/java/virtual_pet/VirtualPetShelter.java
@@ -1,4 +1,12 @@
 package virtual_pet;
 
+import java.util.ArrayList;
+
 public class VirtualPetShelter {
+    ArrayList<String> pets = new ArrayList<String>();
+    String name = "Jeff";
+    myList.add("Jeff");
+
+
+
 }
--- src/test/java/virtual_pet/VirtualPetShelterTest.java
+++ src/test/java/virtual_pet/VirtualPetShelterTest.java
@@ -2,10 +2,29 @@ package virtual_pet;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetShelterTest {
     @Test
     public void shouldBeAbleToCreatePetShelter() {
         VirtualPetShelter shelter = new VirtualPetShelter();
-
     }
+
+    @Test
+    public void petShouldBeAbleToIntakeManyPets() {
+       ArrayList<String> pets = new ArrayList<String>();
+       pets.set(0,"Jeff");
+
+       String result= pets.get(0);
+
+       assertEquals("Jeff", result);
+
+
+        // VirtualPet underTest = new VirtualPet("Jeff", 10, 10, 10, "Taurus");
+        //String result = underTest.getName();
+       // assertEquals("Jeff", result);
+    }
+
 }
