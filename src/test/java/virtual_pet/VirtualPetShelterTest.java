e270fb2993b9bd8ae0109e7ec1eda9393b2a3a80
leviflint <tweekex@hotmail.com>
2020-09-18 13:13:27 -0400

    Pet Intake from Virtual Pet test passed

--- src/test/java/virtual_pet/VirtualPetShelterTest.java
+++ src/test/java/virtual_pet/VirtualPetShelterTest.java
@@ -13,16 +13,21 @@ public class VirtualPetShelterTest {
     }
 
     @Test
-    public void petShouldBeAbleToIntakeManyPets() {
-       ArrayList<String> pets = new ArrayList<String>();
-       pets.add("Jeff");
-       String result= pets.get(0);
-       assertEquals("Jeff", result);
-
-
-        // VirtualPet underTest = new VirtualPet("Jeff", 10, 10, 10, "Taurus");
-        //String result = underTest.getName();
-       // assertEquals("Jeff", result);
+    public void petShouldBeAbleToIntakePets() {
+        ArrayList<String> pets = new ArrayList<String>();
+        VirtualPet underTest = new VirtualPet("Pat", 10, 10, 10, "Taurus");
+        String name = underTest.getName();
+        pets.add(name);
+        String result = pets.get(0);
+        assertEquals("Pat", result);
     }
 
+//    @Test
+//    public void petShouldBeAbleToIntakeManyPets() {
+//        ArrayList<String> pets = new ArrayList<String>();
+//        pets.add("Jeff");
+//        String result = pets.get(0);
+//        assertEquals("Jeff", result);
+//
+//    }
 }
