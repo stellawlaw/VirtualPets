10590815acfba20ddcae8419dda6ded81b5de9ba
leviflint <tweekex@hotmail.com>
2020-09-18 14:32:16 -0400

    End of day 1 Sprint 2

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Application {
     public static void main (String[]args){
         Scanner input= new Scanner(System.in);
+        VirtualPet pet1 = new VirtualPet("Jeff", 10, 10, 10, "Aries");
+        VirtualPet pet2 = new VirtualPet("Stella", 10, 10, 10, "Leo");
 
-        System.out.println("What is your pet's name?");
-        String name = input.nextLine();
-        VirtualPet pet = new VirtualPet(name, 10, 10, 10, "Aries");
-
-        System.out.println("Your pet's name is " + name);
+//        System.out.println("What is your pet's name?");
+//        String name = input.nextLine();
+//        VirtualPet pet = new VirtualPet(name, 10, 10, 10, "Aries");
+//
+//        System.out.println("Your pet's name is " + name);
     }
 
 
--- src/test/java/virtual_pet/VirtualPetShelterTest.java
+++ src/test/java/virtual_pet/VirtualPetShelterTest.java
@@ -13,33 +13,16 @@ public class VirtualPetShelterTest {
     }
 
     @Test
-    public void petShouldBeAbleToIntakePets() {
-        ArrayList<String> pets = new ArrayList<String>();
-        VirtualPet underTest = new VirtualPet("Pat", 10, 10, 10, "Taurus");
-        String name = underTest.getName();
-        pets.add(name);
-        String result = pets.get(0);
-        assertEquals("Pat", result);
+    public void petShelterShouldBeAbleToIntakePets() {
+        VirtualPetShelter shelter = new VirtualPetShelter();
+        VirtualPet pet = new VirtualPet("Pat", 10, 10, 10, "Taurus");
+        shelter.add(pet);
     }
 
-    @Test
-    public void petShouldBeAbleToIntakeManyPets() {
-        ArrayList<String> pets = new ArrayList<String>();
-        VirtualPet pet1 = new VirtualPet("Jeff", 10, 10, 10, "Aries");
-        VirtualPet pet2 = new VirtualPet("Stella", 10, 10, 10, "Leo");
-        pets.add(pet1.getName());
-        pets.add("" + pet1.getHunger());
-        pets.add("" + pet1.getThirst());
-        pets.add("" + pet1.getBoredom());
-        pets.add(pet1.getSign());
-        pets.add(pet2.getName());
-        pets.add("" + pet2.getHunger());
-        pets.add("" + pet2.getThirst());
-        pets.add("" + pet2.getBoredom());
-        pets.add(pet2.getSign());
-        String result = pets.get(0);
-        String result1 = pets.get(5);
-        assertEquals("Jeff", result);
-        assertEquals("Stella", result1);
-    }
+//    @Test
+//    public void petShouldBeAbleToIntakeManyPets() {
+//        ArrayList<String> pets = new ArrayList<String>();
+//        VirtualPet pet1 = new VirtualPet("Jeff", 10, 10, 10, "Aries");
+//        assertEquals("Stella", result);
+//    }
 }
