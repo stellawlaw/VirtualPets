b2f5ab374905f2b9f60c61d7f3c252d26cdfd443
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-09-18 10:50:10 -0400

    Created Virtual Pet Shelter

---
+++ src/test/java/virtual_pet/VirtualPetShelterTest.java
@@ -0,0 +1,11 @@
+package virtual_pet;
+
+import org.junit.jupiter.api.Test;
+
+public class VirtualPetShelterTest {
+    @Test
+    public void shouldBeAbleToCreatePetShelter() {
+        VirtualPetShelter shelter = new VirtualPetShelter();
+
+    }
+}
