0f6db52c64da4e5f5e9f35ce9772609df2b76820
Lauren Monowar-Jones <lvjones214@aol.com>
2020-09-21 15:26:28 -0400

    not sure where we are...

--- src/main/java/virtual_pet/VirtualPetShelter.java
+++ src/main/java/virtual_pet/VirtualPetShelter.java
@@ -1,13 +1,21 @@
 package virtual_pet;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class VirtualPetShelter {
-    Map<String, String> pet;
-    public void add(VirtualPet pet) {
+    public Object getPet;
+    private Collection<VirtualPet> petsList = new Map<>();
+
+    public void addPet(VirtualPet pet) {
+        petsList.add(pet)
     }
+    public Collection<petsList> getPets() {
+        return petsList
+    }
+
+ //   public void getPet(String pet1) {
+ //       return String pet = shelter.get("pet1");
+ //   }
 
 //        ArrayList<String> pets = new ArrayList<String>();
 }
--- src/test/java/virtual_pet/VirtualPetShelterTest.java
+++ src/test/java/virtual_pet/VirtualPetShelterTest.java
@@ -3,7 +3,12 @@ package virtual_pet;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VirtualPetShelterTest {
@@ -15,14 +20,21 @@ public class VirtualPetShelterTest {
     @Test
     public void petShelterShouldBeAbleToIntakePets() {
         VirtualPetShelter shelter = new VirtualPetShelter();
-        VirtualPet pet = new VirtualPet("Pat", 10, 10, 10, "Taurus");
-        shelter.add(pet);
+        VirtualPet pet1 = new VirtualPet("Pat", 10, 10, 10, "Taurus");
+        shelter.addPet(pet1);
+        Object foundPet = shelter.getPet;
+        assertThat(shelter).is(foundPet);
+
     }
 
 //    @Test
 //    public void petShouldBeAbleToIntakeManyPets() {
-//        ArrayList<String> pets = new ArrayList<String>();
-//        VirtualPet pet1 = new VirtualPet("Jeff", 10, 10, 10, "Aries");
+//        VirtualPetShelter shelter = new VirtualPetShelter();
+//        VirtualPet pet1 = new VirtualPet("Pat", 10, 10, 10, "Taurus");
+//        VirtualPet pet2 = new VirtualPet("Stella", 10, 10, 10, "Aquarius");
+//        shelter.add(pet1);
+//        shelter.add(pet2);
+//
 //        assertEquals("Stella", result);
-//    }
+    }
 }
